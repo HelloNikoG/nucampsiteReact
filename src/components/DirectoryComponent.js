3193a9747762016fb1c22bd3998683a5dddc3c86
Niko Garalde <nikog.nyc@gmail.com>
2021-06-30 00:47:21 -0700

    React Router Parameters

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 //render each card with different campsite details
 //destructure props object
-function RenderDirectoryItem({campsite, onClick}){
+function RenderDirectoryItem({campsite}){
         return (
-        <Card onClick = {() => onClick(campsite.id)}>
-            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-            <CardImgOverlay>
-                <CardTitle>{campsite.name}</CardTitle>
-            </CardImgOverlay>
+        <Card>
+            <Link to={`/directory/${campsite.id}`}>
+                <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                <CardImgOverlay>
+                    <CardTitle>{campsite.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
         </Card>
             
 
@@ -24,7 +27,7 @@ function Directory (props){
     const directory = props.campsites.map(campsite => {
         return (
             <div key={campsite.id} className="col-md-5 m-1">
-                <RenderDirectoryItem campsite = {campsite} onClick={props.onClick} />
+                <RenderDirectoryItem campsite = {campsite} />
             </div> 
         );
     });   
